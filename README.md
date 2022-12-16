b770dd9653ecfca8b022de74471ee6d509cb7335
Victor <victorkongym@gmail.com>
2022-12-16 17:29:57 +0000

    first version mockup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 [Reborn App](https://uahh.site/reborn)
+
+Births by country data pulled from Wiki page on 16 Dec 2022 (data as of 2021): https://en.wikipedia.org/wiki/List_of_countries_by_number_of_births
+
+Converted data to JSON using: https://www.convertcsv.com/csv-to-json.html
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+// Packages used:
+// 1) React
+// 2) Material UI (Core)
+
+// To implement:
+// 1) Map UI - react leaflet to add
+// 2)
+
+// Note:
+// 1) Birth data - https://en.wikipedia.org/wiki/List_of_countries_by_number_of_births
+
 import React from "react";
+import Button from "@mui/material/Button";
+import TablePlaceHolder from "./components/Table";
+import Map from "./components/Map";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1>Title here</h1>
+      <Map attributionControl={false} />
+      <Button variant="contained">Refresh Data</Button>
+      <TablePlaceHolder />
+      <Button variant="contained">Share as jpg button</Button>
+      <Button variant="contained">Share to social media button</Button>
+    </div>
+  );
 }
 
 export default App;
