b72f05fc54586b26c2ca2045de2a7a66af5d4208
Victor <victorkongym@gmail.com>
2022-12-20 15:38:39 +0000

    MVP complete

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
+<!--
+TODO:
+1) Map data in country location and birth by country JSON files
+2) Randomise location based on probability
+3) Responsive interface
+4) Add export feature (png or share with friends)
+5) Stimulation feature (dropdown menu)
+6) Update Table header (visual)
+99) Modulise components
+
+Packages used:
+1) React
+2) Material UI (Core)
+3) React Leaflet
+
+Note:
+1) Birth data - https://en.wikipedia.org/wiki/List_of_countries_by_number_of_births
+2) Country location - https://www.mapsofworld.com/world-maps/world-map-with-latitude-and-longitude.html
+-->
+
 [Reborn App](https://uahh.site/reborn)
 
 Births by country data pulled from Wiki page on 16 Dec 2022 (data as of 2021): https://en.wikipedia.org/wiki/List_of_countries_by_number_of_births
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,89 @@
-// TODO:
-// 1) Map data in country location and birth by country JSON files
-// 2) Capture data in table
-
-// Packages used:
-// 1) React
-// 2) Material UI (Core)
-// 3) Map UI - react leaflet to add
-
-// Note:
-// 1) Birth data - https://en.wikipedia.org/wiki/List_of_countries_by_number_of_births
-// 2) Country location - https://www.mapsofworld.com/world-maps/world-map-with-latitude-and-longitude.html
-
-import React from "react";
-import TablePlaceHolder from "./components/Table";
-import MapWidget from "./components/MapWidget";
-// import Button from "@mui/material/Button";
-// import LocationFilter from "./components/LocationFilter";
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import { useMap, MapContainer, TileLayer, Marker } from "react-leaflet";
+import data from "./data/location_list.json";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  // Array to store country records
+  const [countryRecord, setCountryRecord] = useState([]);
+  // Set Map Widget:
+  const [pinLocation, setPinLocation] = useState([5, 5]);
+  const randomCountryIndex = Math.floor(Math.random() * data.length);
+  const randomCountryName = data[randomCountryIndex].Country;
+  const randomCountryLatitude = data[randomCountryIndex].Latitude;
+  const randomCountryLongitude = data[randomCountryIndex].Longitude;
+  // Set Map Pin Location
+  function handlePinLocation() {
+    setPinLocation([randomCountryLatitude, randomCountryLongitude]);
+    setCountryRecord(prevCountryRecord => [
+      ...prevCountryRecord,
+      randomCountryName,
+    ]);
+  }
+  // Handle auto panning based on pin location
+  function UpdateMapCentre(props) {
+    const map = useMap();
+    map.panTo(props.mapCentre, {
+      animate: true,
+      duration: 1,
+      easeLinearity: 0.5,
+    });
+    return null;
+  }
+
   return (
-    <div>
+    <>
       <h1>Title here</h1>
-      {/* <Button variant="contained">Share as jpg (WIP) </Button> */}
-      {/* <Button variant="contained">Share to social media (WIP)</Button> */}
-      {/* <br /> */}
-      {/* <LocationFilter /> */}
-      {/* <Button variant="contained">Filter location (WIP)</Button> */}
-      <MapWidget />
-      <TablePlaceHolder />
-    </div>
+      {/* Map Component */}
+      <>
+        <MapContainer center={[0, 0]} zoom={1.5} attributionControl={false}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Marker position={pinLocation}></Marker>
+          <UpdateMapCentre mapCentre={pinLocation} />
+        </MapContainer>
+        <Button onClick={handlePinLocation} variant="contained">
+          Refresh Data
+        </Button>
+      </>
+      {/* Table */}
+      <>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <colgroup>
+              <col style={{ width: "30%" }} />
+              <col style={{ width: "70%" }} />
+            </colgroup>
+            <TableHead>
+              <TableRow>
+                <TableCell>Reroll</TableCell>
+                <TableCell>Country</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {countryRecord.map((country, index) => (
+                <TableRow
+                  key={uuidv4()}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {index + 1}
+                  </TableCell>
+                  <TableCell>{country}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </>
+    </>
   );
 }
 
