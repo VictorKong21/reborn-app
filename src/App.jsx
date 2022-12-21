5f165767157b7f963c74dbc896de68206dfd063a
Victor <victorkongym@gmail.com>
2022-12-21 11:42:45 +0000

    update table format (bug to fix)

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
 import React, { useState } from "react";
 import { useMap, MapContainer, TileLayer, Marker } from "react-leaflet";
 import data from "./data/location_list.json";
-import {
-  Button,
-  Box,
-  Stack,
-  styled,
-  Table,
-  TableBody,
-  TableCell,
-  tableCellClasses,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-  Paper,
-} from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import { v4 as uuidv4 } from "uuid";
 
 function App() {
   // Array to store country records
@@ -35,6 +20,13 @@ function App() {
       ...prevCountryRecord,
       randomCountryName,
     ]);
+    // ******* BUG: First result doesn't show up after first click!!
+    setCountryRecordObj(
+      countryRecord.map((country, index) => ({
+        id: index + 1,
+        country,
+      }))
+    );
   }
   // Handle auto panning based on pin location
   function UpdateMapCentre(props) {
@@ -46,34 +38,15 @@ function App() {
     });
     return null;
   }
-  // Table header formatting
-  const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.primary.main,
-      color: theme.palette.primary.contrastText,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-  }));
 
   // Table 2
   const columns = [
-    { field: "id", headerName: "ID", width: 50 },
-    { field: "lastName", headerName: "Last name", width: 130 },
+    { field: "id", headerName: "Reroll", width: 50 },
+    { field: "country", headerName: "Country", width: 130 },
   ];
 
-  const rows = [
-    { id: 1, lastName: "Snow" },
-    { id: 2, lastName: "Lannister" },
-    { id: 3, lastName: "Lannister" },
-    { id: 4, lastName: "Stark" },
-    { id: 5, lastName: "Targaryen" },
-    { id: 6, lastName: "Melisandre" },
-    { id: 7, lastName: "Clifford" },
-    { id: 8, lastName: "Frances" },
-    { id: 9, lastName: "Roxie" },
-  ];
+  // Table2 testing
+  const [countryRecordObj, setCountryRecordObj] = useState([]);
 
   return (
     <Stack alignItems="center" spacing={2}>
@@ -91,42 +64,11 @@ function App() {
         Refresh Data
       </Button>
 
-      {/* Table */}
-      <Box sx={{ width: "50%" }}>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} size="small">
-            <colgroup>
-              <col style={{ width: "30%" }} />
-              <col style={{ width: "70%" }} />
-            </colgroup>
-            <TableHead>
-              <TableRow>
-                <StyledTableCell align="center">Reroll</StyledTableCell>
-                <StyledTableCell align="center">Country</StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {countryRecord.map((country, index) => (
-                <TableRow
-                  key={uuidv4()}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row" align="center">
-                    {index + 1}
-                  </TableCell>
-                  <TableCell align="center">{country}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Box>
-
       {/* Table2 */}
       <div style={{ height: 400, width: "100%" }}>
         <DataGrid
           disableColumnMenu
-          rows={rows}
+          rows={countryRecordObj}
           columns={columns}
           // pageSize={5}
           rowsPerPageOptions={[5]}
